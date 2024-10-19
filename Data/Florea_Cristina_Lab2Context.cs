f77d397d2d29c13f881b8e4afa11e85f17a62f61
CristinaFlorea15 <cristinafloreasb@gmail.com>
2024-10-19 02:37:38 -0700

    lab2

---
+++ Data/Florea_Cristina_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Florea_Cristina_Lab2.Models;
+
+namespace Florea_Cristina_Lab2.Data
+{
+    public class Florea_Cristina_Lab2Context : DbContext
+    {
+        public Florea_Cristina_Lab2Context (DbContextOptions<Florea_Cristina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Florea_Cristina_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Florea_Cristina_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
